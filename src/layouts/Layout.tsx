4b52018496835d0b363fe1466e19ffa3a082c2fa
Ben-plt <benjamin@entrepreneuraligne.fr>
2026-04-07 20:08:48 +0700

    fix(layout): add global overflow-x-hidden to prevent mobile horizontal scrolling jitter

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,7 +5,7 @@ import { StickyMobileCTA } from '../components/StickyMobileCTA';
 
 export function Layout() {
     return (
-        <div className="min-h-screen bg-black text-cream selection:bg-wine selection:text-white flex flex-col">
+        <div className="min-h-screen w-full overflow-x-hidden bg-black text-cream selection:bg-wine selection:text-white flex flex-col relative">
             <Navbar />
             <main className="flex-grow">
                 <Outlet />
